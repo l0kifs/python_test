765ffe8b86cc11d98abba94a51b74ec09923fd64
l0kifs <l0kifs91@gmail.com>
2022-11-24 13:00:36 +0300

    *

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,38 @@
+import os
+
+from peewee import MySQLDatabase, Model, CharField
+
+
 def test_positive():
     assert 1==1
 
 
 def test_negative():
     assert 1==2
+
+
+def test_connect_db():
+    db = MySQLDatabase(None)
+
+    class BaseModel(Model):
+        class Meta:
+            database = db
+
+    class Iban(BaseModel):
+        iban = CharField(unique=True)
+        swift = CharField()
+        country_code = CharField()
+        beneficiary_country = CharField()
+
+    db.init('qa-tests',
+            user='qa-tests',
+            password=os.environ.get("PASS", ""),
+            host=os.environ.get("HOST", ""),
+            port=3306)
+
+    iban = Iban.select().first()
+    print('ibans =>', iban.swift)
+
+    db.close()
+
+
